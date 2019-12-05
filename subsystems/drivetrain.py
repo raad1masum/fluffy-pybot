11154530fd91e9fe04929dc2a834e867099f02a8
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2019-12-04 20:17:26 -0800

    create motor speed groups

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -3,6 +3,7 @@ import wpilib
 from ctre import ControlMode
 from ctre import WPI_TalonSRX
 from wpilib import SpeedControllerGroup
+from wpilib.drive import DifferentialDrive
 
 import navx
 
@@ -16,15 +17,19 @@ def run():
 
 class Drivetrain(self):
 
+    # initialize wpilib stuff
+    self.sd = wpilib.SmartDashboard
+    self.timer = wpilib.Timer()
+
     # initialize motors
     self.motorLeftFront = robotMap.motorLeftFront
     self.motorLeftMiddle = robotMap.motorLeftMiddle
     self.motorLeftBack = robotMap.motorLeftBack
-    self.left = wpilib.SpeedControllerGroup(robotMap.motorLeftFront, robotMap.motorLeftMiddle, robotMap.motorLeftBack)
+    self.leftSideMotors = wpilib.SpeedControllerGroup(robotMap.motorLeftFront, robotMap.motorLeftMiddle, robotMap.motorLeftBack)
     self.motorRightFront = robotMap.motorRightFront
     self.motorRightMiddle = robotMap.motorRightMiddle
     self.motorRightBack = robotMap.motorRightBack
-    self.right = wpilib.SpeedControllerGroup(robotMap.motorRightFront, robotMap.motorRightMiddle, robotMap.motorRightBack)
+    self.rightSideMotors = wpilib.SpeedControllerGroup(robotMap.motorRightFront, robotMap.motorRightMiddle, robotMap.motorRightBack)
 
     # initialize drive sticks
     self.leftStick = robotMap.xboxControllerLeftStickY
@@ -34,29 +39,39 @@ class Drivetrain(self):
     self.navx = navx.AHRS.create_spi()
     self.analog = wpilib.AnalogInput(navx.getNavxAnalogInChannel(robotMap.gyro))
 
-    def gyroDisabled(self):
+    def motors(self):
 
-        self.logger.info("Entered disabled mode")
+        def speedLeftMotors(self, speed):
+            Drivetrain.leftSideMotors.set(ControlMode.PercentOutput, -speed)
+            
+        def speedRightMotors(self):
+            Drivetrain.rightSideMotors.set(ControlMode.PercentOutput, speed)
 
-        self.timer.reset()
-        self.timer.start()
+    def gyro(self):
 
-        while self.isDisabled():
+        def gyroDisabled(self):
 
-            if self.timer.hasPeriodPassed(0.5):
-                self.sd.putBoolean(
-                    "SupportsDisplacement", self.navx._isDisplacementSupported()
-                )
-                self.sd.putBoolean("IsCalibrating: ", self.navx.isCalibrating())
-                self.sd.putBoolean("IsConnected: ", self.navx.isConnected())
-                self.sd.putNumber("Angle: ", self.navx.getAngle())
-                self.sd.putNumber("Pitch: ", self.navx.getPitch())
-                self.sd.putNumber("Yaw: ", self.navx.getYaw())
-                self.sd.putNumber("Roll: ", self.navx.getRoll())
-                self.sd.putNumber("Analog: ", self.analog.getVoltage())
-                self.sd.putNumber("Timestamp: " , self.navx.getLastSensorTimestamp())
+            self.logger.info("Entered disabled mode")
 
-            wpilib.Timer.delay(0.010)
+            self.timer.reset()
+            self.timer.start()
+
+            while self.isDisabled():
+
+                if self.timer.hasPeriodPassed(0.5):
+                    self.sd.putBoolean(
+                        "SupportsDisplacement", self.navx._isDisplacementSupported()
+                    )
+                    self.sd.putBoolean("IsCalibrating: ", self.navx.isCalibrating())
+                    self.sd.putBoolean("IsConnected: ", self.navx.isConnected())
+                    self.sd.putNumber("Angle: ", self.navx.getAngle())
+                    self.sd.putNumber("Pitch: ", self.navx.getPitch())
+                    self.sd.putNumber("Yaw: ", self.navx.getYaw())
+                    self.sd.putNumber("Roll: ", self.navx.getRoll())
+                    self.sd.putNumber("Analog: ", self.analog.getVoltage())
+                    self.sd.putNumber("Timestamp: " , self.navx.getLastSensorTimestamp())
+
+                wpilib.Timer.delay(0.010)
 
 if __name__ == "__main__":
     wpilib.run(Drivetrain())
