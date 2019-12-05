64bd1b8ea471f3e5ab8c67536914855db77d0fe2
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2019-12-04 19:29:25 -0800

    add code to robot.py

--- robot.py
+++ robot.py
@@ -1,1 +1,45 @@
+import magicbot
 import wpilib
+import wpilib.drive
+import math
+import navx
+
+import robot_map
+
+class Robot(magicbot.MagicRobot):
+    def robotPeriodic(self):
+        """
+        Executed periodically regardless of mode.
+        """
+        pass
+
+    def autonomous(self):
+        """
+        Prepare for and start autonomous mode.
+        """
+        # Call autonomous
+        super().autonomous()
+
+    def disabledInit(self):
+        """
+        Executed once right away when robot is disabled.
+        """
+        # Reset Gyro to 0
+        self.navx.reset()
+
+    def disabledPeriodic(self):
+        """
+        Executed periodically while robot is disabled.
+        Useful for testing.
+        """
+        pass
+
+    def teleopInit(self):
+        """
+        Executed when teleoperated mode begins.
+        """
+
+    def teleopPeriodic(self):
+        """
+        Executed periodically while robot is in teleoperated mode.
+        """
