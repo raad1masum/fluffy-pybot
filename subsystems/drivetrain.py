d40c7cec9aaed71b2cff3f1037d7b3d9ab04e8ba
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2019-12-04 19:53:57 -0800

    create speed controller group

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -5,4 +5,10 @@ from ctre import WPI_TalonSRX
 from wpilib import SpeedControllerGroup
 
 from wpilib.command import Subsystem
+from wpilib.smartdashboard import SmartDashboard
 from subsystems.drivetrain import robotMap
+from commands.tank_drive import *
+
+class Drivetrain(self):
+    self.left = wpilib.SpeedControllerGroup(robotMap.motorLeftFront, robotMap.motorLeftMiddle, robotMap.motorLeftBack)
+    self.right = wpilib.SpeedControllerGroup(robotMap.motorRightFront, robotMap.motorRightMiddle, robotMap.motorRightBack)
