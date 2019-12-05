045e1cab9c503e76b5f298cc9619cde091269129
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2019-12-04 19:56:12 -0800

    initialize motors

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -10,5 +10,13 @@ from subsystems.drivetrain import robotMap
 from commands.tank_drive import *
 
 class Drivetrain(self):
+    
+    # initialize motors
+    self.motorLeftFront = robotMap.motorLeftFront
+    self.motorLeftMiddle = robotMap.motorLeftMiddle
+    self.motorLeftBack = robotMap.motorLeftBack
     self.left = wpilib.SpeedControllerGroup(robotMap.motorLeftFront, robotMap.motorLeftMiddle, robotMap.motorLeftBack)
+    self.motorRightFront = robotMap.motorRightFront
+    self.motorRightMiddle = robotMap.motorRightMiddle
+    self.motorRightBack = robotMap.motorRightBack
     self.right = wpilib.SpeedControllerGroup(robotMap.motorRightFront, robotMap.motorRightMiddle, robotMap.motorRightBack)
