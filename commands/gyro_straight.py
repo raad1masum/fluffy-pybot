3ead582face50129320ebbe65301e17a4bd0435b
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2019-12-04 21:57:29 -0800

    gyro

--- commands/gyro_straight.py
+++ commands/gyro_straight.py
@@ -7,6 +7,6 @@ from commandbased import CommandBasedRobot
 from subsystems import drivetrain
 from commands import tank_drive
 
-class gyroStraight():
+class gyroStraight(self):
     if abs(navx.getAngle()) <= 3:
-        
+        tank_drive.TankDrive.tankDriveRobot.tankDrive(robot_map.leftSlow * -1, robot_map.rightSlow * -1)
